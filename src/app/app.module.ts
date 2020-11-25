aff8244af26473cad13b5dc812d8f255b3c080ab
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-11-25 11:58:30 -0300

    Criação do mapa e uso da localização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    Geolocation,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+	map: google.maps.Map;
+	myPosition: google.maps.LatLng;
 
+	@ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+	constructor(private geolocation: Geolocation) { }
+
+	ionViewWillEnter() {
+		this.showMap();
+	}
+
+	showMap() {
+		const position = new google.maps.LatLng(-15.79902, -47.86075);
+		const options = {
+			center: position,
+			zoom: 1,
+			disableDefaultUi: true
+		}
+		this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+		this.findPosition();
+		/*
+		new google.maps.Marker({
+		   position: position,
+		   map: this.map,
+		   title:'Planalto',
+		   animation: google.maps.Animation.BOUNCE			 
+		});
+		*/
+	}
+
+
+
+	findPosition() {
+		this.geolocation.getCurrentPosition().then((resp) => {
+			this.myPosition = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+			this.goToMyPosition();
+		}).catch((error) => {
+			console.log("Error", error);
+		});
+	}
+
+	goToMyPosition() {
+		this.map.setCenter(this.myPosition);
+		this.map.setZoom(15);
+
+		new google.maps.Marker({
+			position: this.myPosition,
+			map: this.map,
+			title: 'Minha Posição',
+			animation: google.maps.Animation.BOUNCE
+		});
+	}
 }
