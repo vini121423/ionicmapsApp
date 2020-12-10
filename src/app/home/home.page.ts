b6163e0c3833d6803478b3c35ccb8bd1d3420294
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-12-10 20:08:20 -0300

    Criação de rotas usando APIs do Google Maps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
@@ -9,11 +9,16 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 export class HomePage {
 
+	private direction = new google.maps.DirectionsService();
+	private directionsRender = new google.maps.DirectionsRenderer();
+	private autocomplete = new google.maps.places.AutocompleteService
 	map: google.maps.Map;
 	myPosition: google.maps.LatLng;
+	listAddress: any = [];
 
 	@ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
-	constructor(private geolocation: Geolocation) { }
+	constructor(private geolocation: Geolocation,
+		private ngZone: NgZone) { }
 
 	ionViewWillEnter() {
 		this.showMap();
@@ -59,5 +64,57 @@ export class HomePage {
 			title: 'Minha Posição',
 			animation: google.maps.Animation.BOUNCE
 		});
+	}
+
+	findPlaces(event: any) {
+		const busca = event.target.value as string;
+		if (!busca.trim().length) {
+			this.listAddress = [];
+			return false;
+		}
+
+		this.autocomplete.getPlacePredictions({ input: busca }, (localArrays, status) => {
+
+			if (status == 'OK') {
+				this.ngZone.run(() => {
+					this.listAddress = localArrays;
+				});
+			} else {
+				this.listAddress = [];
+			}
+		});
+	}
+
+	public createRoute(local: google.maps.places.AutocompletePrediction) {
+		this.listAddress = [];
+
+		new google.maps.Geocoder().geocode({ address: local.description }, resultado => {
+			//this.map.setCenter(resultado[0].geometry.location);
+			//this.map.setZoom(19);
+
+			const route: google.maps.DirectionsRequest = {
+				origin: this.myPosition,
+				destination: resultado[0].geometry.location,
+				unitSystem: google.maps.UnitSystem.METRIC,
+				travelMode: google.maps.TravelMode.DRIVING
+
+			};
+
+			this.direction.route(route, (result, status) => {
+				if (status == 'OK') {
+					this.directionsRender.setMap(this.map);
+					this.directionsRender.setDirections(result);
+					this.directionsRender.setOptions({ suppressMarkers: true })
+				}
+			});
+
+			const maker = new google.maps.Marker({
+				position: resultado[0].geometry.location,
+				title: resultado[0].formatted_address,
+				animation: google.maps.Animation.DROP,
+				map: this.map
+
+			})
+		})
 	}
 }
